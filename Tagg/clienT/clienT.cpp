08c1d5761fccb141bc0dc83ac0378e4c89e80083
therogue <alex@panait.com>
2016-10-30 09:21:40 -0400

    Hello world base program. Causes an error on my machine.

---
+++ Tagg/clienT/clienT.cpp
@@ -0,0 +1,14 @@
+// clienT.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Test execution complete. Missing database library.";
+
+    return 0;
+}
+
